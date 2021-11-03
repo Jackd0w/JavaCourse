6e7ed49a8cec835edb49d9cfca99fc1ce196be6c
laughing man <komarovandrey396@gmail.com>
2021-11-03 22:36:35 +0300

    added Frame drawer function

--- laba5/main.java
+++ laba5/main.java
@@ -1,3 +1,21 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.*;
+
+public void paintComponent(Graphics g) {
+    Graphics2D g2 = (Graphics2D) g;
+
+}
+
 public class main {
-    
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> 
+        {
+            var frame = new DrawFrame();
+            frame.setTitle("Figure")
+            frame.setDefaultCloseOperation(
+                JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
 }
