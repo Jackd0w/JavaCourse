70a4e1f2a94f928ea16b69285191a1f2de000686
laughing man <komarovandrey396@gmail.com>
2021-11-18 20:28:49 +0300

    added sth

--- laba5/RandomColour.java
+++ laba5/RandomColour.java
@@ -4,7 +4,7 @@ public class RandomColour {
     public static Color get() {
         Random random = new Random();
     // Массив из пяти цветов
-        int colors[] = { Color.BLUE, Color.GREEN, Color.MAGENTA, Color.RED,
+        Color[] colors = { Color.BLUE, Color.GREEN, Color.MAGENTA, Color.RED,
                 Color.CYAN };
         int pos = random.nextInt(colors.length);
     }
